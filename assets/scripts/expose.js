56eeb8ff651e2620af813a28e112b3f77cf80811
rdshelopal <rdshelopal@gmail.com>
2024-05-06 20:49:53 -0700

    setup

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,7 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let horn = document.getElementById('horn-select');
+  let image = document.getElementById('image-container');
+  horn.addEventListener('click')
 }
